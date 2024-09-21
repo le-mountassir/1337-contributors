3aaa20e487e6c57fe4a19fbbfb45a3a992d88849
MliliGenes <saad.elm.77@gmail.com>
2024-09-21 03:33:17 +0100

    -UI_update -users_fetching_update -default_avatars -tailwindcss:)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,16 @@
 import React, { FC } from "react";
-import "./layout.css";
+import logo from "@assets/logo.svg";
 
 const Layout: FC = ({ children }) => {
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+      <div className="bg-gray-900 text-white font-Roboto">
+        <div className="h-24 flex items-center justify-center ">
+          <div className="relative" id="logo">
+            <img src={logo} title="1337 Contributors" />
+          </div>
+        </div>
+        <main className="text-white">{children}</main>
       </div>
     </>
   );
